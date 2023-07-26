35106b32474a278e02e6bdafecab8b5859f899b4
ZeNyfh <66276905+ZeNyfh@users.noreply.github.com>
2023-07-26 14:15:34 +0100

    fix capitalisations
    This caused some issues on newer forge versions.

--- src/main/java/org/bhop/items/BhopCreativeModeTabs.java
+++ src/main/java/org/bhop/items/BhopCreativeModeTabs.java
@@ -12,8 +12,8 @@ import org.bhop.blocks.BhopBlocks;
 public class BhopCreativeModeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Bhop.MODID);
 
-    public static final RegistryObject<CreativeModeTab> BHOP_TAB = CREATIVE_MODE_TABS.register("Bhop",
-            () -> CreativeModeTab.builder().icon(() -> new ItemStack(BhopBlocks.CHECKPOINT_TRIGGER.get())).title(Component.translatable("creativetab.Bhop_tab"))
+    public static final RegistryObject<CreativeModeTab> BHOP_TAB = CREATIVE_MODE_TABS.register("bhop",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(BhopBlocks.CHECKPOINT_TRIGGER.get())).title(Component.translatable("creativetab.bhop_tab"))
                     .displayItems((pParameters, pOutput) -> {
                         pOutput.accept(BhopBlocks.KILL_TRIGGER.get());
                         pOutput.accept(BhopBlocks.SPAWN_TRIGGER.get());
